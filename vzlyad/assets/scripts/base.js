b77024ad55a6e9f1d17d6b54098075c59229c85a
AntonKovalev1 <prsarx@gmail.com>
2024-02-13 18:46:55 +0400

    Новый взгляд
    первый коммит

---
+++ vzlyad/assets/scripts/base.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+
+
+
+
+
+
+    var swiper = new Swiper(".slider-images", {
+
+        createElements: true,
+
+        pagination: {
+            el: ".swiper-pagination"
+        },
+
+        lazy: true,
+
+        speed: 200,
+        spaceBetween: 0,
+
+    });
+
+
+});
