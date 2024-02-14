2b7a1d61ff6dccf7f032ad3f6812161036ccd8af
AntonKovalev1 <prsarx@gmail.com>
2024-02-14 14:49:17 +0400

    menu

--- vzlyad/assets/scripts/base.js
+++ vzlyad/assets/scripts/base.js
@@ -2,6 +2,91 @@ $(document).ready(function(){
 
 
 
+    function closePopupmenu() {
+        $('.popupmenu-after').addClass('popupmenu-after-display');
+        setTimeout(function(){
+            $('.popupmenu-after').removeClass('popupmenu-after-display');
+        }, 200);
+    }
+
+
+
+
+    $('.header-burger-a:eq(0)').click(function(){
+        if (!$('#popupmenu-left-body').attr('popupmenu-left-body')) {
+            $('#popupmenu-left-body').append($('.footer-wrap').parent().html());
+            $('#popupmenu-left-body').attr('popupmenu-left-body', true);
+        }
+
+        $('body').addClass('popupmenu-left-open');
+
+       return false;
+    });
+
+    function closePopupmenuLeft() {
+        $('body').removeClass('popupmenu-left-open');
+        closePopupmenu();
+    }
+
+    $('.popupmenu-left-close:eq(0)').click(function () {
+        closePopupmenuLeft();
+    });
+
+    /*
+    $('.popupmenu-after:eq(0)').click(function () {
+        closePopupmenu();
+    });
+    */
+
+
+
+
+
+
+    $('.button-menu-right:eq(0)').click(function(){
+        $('body').addClass('popupmenu-right-open');
+    });
+
+    function closePopupmenuRight() {
+        $('body').removeClass('popupmenu-right-open');
+        closePopupmenu();
+    }
+
+    $('.popupmenu-right-close:eq(0)').click(function () {
+        closePopupmenuRight();
+    });
+
+    /*
+     $('.popupmenu-after:eq(0)').click(function () {
+     closePopupmenu();
+     });
+     */
+
+
+    /*
+
+    let x;
+    addEventListener('touchstart', e => x = e.changedTouches[0].clientX);
+    addEventListener('touchend', e => e.changedTouches[0].clientX - x < -50 && swipeLeft());
+
+    function swipeLeft() {
+        closePopupmenu();
+    }
+
+    */
+
+    /*
+    addEventListener('touchstart', () => { console.log('start') });
+    addEventListener('touchend', () => { console.log('end') });
+    addEventListener('touchmove', () => { console.log('move') });
+    addEventListener('touchcancel', () => { console.log('cancel') });
+    */
+
+
+    $('.header-burger-a:eq(0)').click();
+
+
+
 
 
 
